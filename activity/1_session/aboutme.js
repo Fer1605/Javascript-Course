c823f99c114ab0c6ebb8411d5fdbf03faf7ab0c2
Fer Santizo <f.160511@gmail.com>
2025-04-22 13:37:16 -0600

    Agrega script aboutme.js con presentación

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,10 @@
-console.log("hello world");
+const nombreCompleto = "Marcela Fernanda Valle Santizo";
+const profesion = "Front-End Developer";
+const puestoDeseado = "Full-Stack Developer";
 
+const presentacion = `Hola, me llamo ${nombreCompleto} y me dedico a ${profesion}.
+Estoy cursando este Máster porque me gustaría trabajar como ${puestoDeseado}.`;
 
-console.log("laater");
+console.log(presentacion);
+
+export default presentacion;
